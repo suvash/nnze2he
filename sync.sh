4ffa2104222fbcd93a9d53aa4948f961cad59a67
Suvash Thapaliya <suvash@gmail.com>
2023-05-23 17:52:39 +0200

    finished manual backpropagation notebook

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -razuvh --info=progress2 --exclude '.git' --delete ../deeplab/app/notebooks/nnze2he/ .
+rsync -razuvh --info=progress2 --exclude '.git' --exclude '.ipynb_checkpoints/' --delete ../deeplab/app/notebooks/nnze2he/ .
