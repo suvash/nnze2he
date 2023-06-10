20602a98d1f132eb31e72e9e5912149c681201c3
Suvash Thapaliya <suvash@gmail.com>
2023-06-10 21:58:26 +0200

    add layer norm

--- makemore/src/lm.py
+++ makemore/src/lm.py
@@ -125,10 +125,12 @@ class Block(nn.Module):
         head_size = n_embed // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
     
     def forward(self, x):
-        x = x + self.sa(x)   # RESIDUAL ADD
-        x = x + self.ffwd(x) # RESIDUAL ADD
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 
@@ -143,6 +145,7 @@ class LanguageModel(nn.Module):
             Block(n_embed, n_head=4),
             Block(n_embed, n_head=4),
             Block(n_embed, n_head=4),
+            nn.LayerNorm(n_embed),
         )
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
