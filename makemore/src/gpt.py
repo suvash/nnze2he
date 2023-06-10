4dd5e98d8c98819a671d94e3a40c20e09f53463b
Suvash Thapaliya <suvash@gmail.com>
2023-06-10 22:36:21 +0200

    all the way to gpt

--- makemore/src/gpt.py
+++ makemore/src/gpt.py
@@ -3,17 +3,21 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+# ------------
+
 print(f'Using device : {device}')
 # to use gpu/device, data and model params has to be moved to the device
-eval_iters = 200
-n_embed = 32
-# ------------
 
 torch.manual_seed(1337)
 
@@ -75,6 +79,7 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embed, head_size, bias=False)
         # in Pytorch convention a variable that's not a parameter of the model is called a buffer
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         B,T,C = x.shape
@@ -85,6 +90,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei) # dropout some of the affinities
         # perform the weighted aggregation of the values
         v = self.value(x) # (B, T, hs)
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
@@ -96,10 +102,12 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.proj(out) # outcome of the linear layer to project back into the residual pathway
+        out = self.dropout(out) # final dropout
         return out
 
 class FeedForward(nn.Module):
@@ -110,7 +118,8 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embed, 4 * n_embed), # as mentioned in the paper
             nn.ReLU(),
-            nn.Linear(4 * n_embed, n_embed) # projection layer : the final projection back into the residual pathway
+            nn.Linear(4 * n_embed, n_embed), # projection layer : the final projection back into the residual pathway
+            nn.Dropout(dropout), # dropout before final projection
         )
         
     def forward(self, x):
@@ -141,12 +150,8 @@ class LanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.blocks = nn.Sequential(
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            nn.LayerNorm(n_embed),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)]) 
+        self.ln_f = nn.LayerNorm(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
     def forward(self, idx, targets=None):
@@ -157,6 +162,7 @@ class LanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = token_emb + pos_emb # (B, T, C)
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)# (B,T,vocab_size)
 
         if targets is None:
@@ -213,4 +219,11 @@ for iter in range(max_iters):
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+output = decode(m.generate(context, max_new_tokens=500)[0].tolist())
+
+# write longer output to file
+output_long = decode(m.generate(context, max_new_tokens=5000)[0].tolist())
+with open('../fakespeare.txt', 'w', encoding='utf-8') as f:
+    f.write(output_long)
+
+print(output)
