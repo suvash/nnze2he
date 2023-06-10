b67bfc1a246a5c2092763100cd998650148adb1e
Suvash Thapaliya <suvash@gmail.com>
2023-06-10 21:08:04 +0200

    starting to become a lm, better call it that

--- makemore/src/lm.py
+++ makemore/src/lm.py
@@ -99,6 +99,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
 
+class FeedForward(nn.Module):
+    " simple linear layer followed by non linearity "
+    
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, n_embed),
+            nn.ReLU(),
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -108,6 +121,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
         self.sa_heads = MultiHeadAttention(4, n_embed//4) # i.e. 4 heads of (32//4)8 dimensional self attention - gives back the same dims
+        self.ffwd = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
     def forward(self, idx, targets=None):
@@ -118,6 +132,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = token_emb + pos_emb # (B, T, C)
         x = self.sa_heads(x) # apply self attention heads
+        x = self.ffwd(x) # (B, T, C)
         logits = self.lm_head(x)# (B,T,vocab_size)
 
         if targets is None:
