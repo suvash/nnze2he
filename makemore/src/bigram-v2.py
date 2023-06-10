a88b4fd064063e5883e59b1e733d2e95b16de077
Suvash Thapaliya <suvash@gmail.com>
2023-06-10 17:28:12 +0200

    add multi headed attention

--- makemore/src/bigram-v2.py
+++ makemore/src/bigram-v2.py
@@ -90,6 +90,15 @@ class Head(nn.Module):
         out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -98,7 +107,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_head = Head(n_embed)
+        self.sa_heads = MultiHeadAttention(4, n_embed//4) # i.e. 4 heads of (32//4)8 dimensional self attention - gives back the same dims
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
     def forward(self, idx, targets=None):
@@ -108,7 +117,7 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = token_emb + pos_emb # (B, T, C)
-        x = self.sa_head(x) # apply one head of self attention
+        x = self.sa_heads(x) # apply self attention heads
         logits = self.lm_head(token_emb)# (B,T,vocab_size)
 
         if targets is None:
